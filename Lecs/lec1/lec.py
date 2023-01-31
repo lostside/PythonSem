061e73c2b59d7f4216f8d33c439a57574e3c9a3f
Ситников Денис Романович <Sitnikov.D.R@sblogistica.ru>
2023-01-31 17:16:34 +0300

    add file lec

---
+++ Lecs/lec1/lec.py
@@ -0,0 +1,46 @@
+
+# n = '123\'1' # Вывод ковычки в строке с помощью \
+
+# print((n))
+
+# print(2)
+# print(2)
+
+
+# print(2)
+# print(2)
+# print(2)
+
+"""
+print(2)
+print(2)
+print(2)
+"""
+
+# a = 5
+# b = 5.89
+# c = 'hello'
+# print(a, ' - ', b, ' - ', c) # 5 - 5.89 - hello
+
+# print(f"{a} - {b} - {c}") # 5 - 5.89 - hello
+
+# print("{} - {} - {}" .format(a,b,c)) #5 - 5.89 - hello
+
+# print('Введите первую строку')
+# a = input()
+# b = input('Введите второе число: ')
+
+# print(a, ' + ', b, ' = ', a + b) = 34
+
+
+# c = 1
+# print(c)
+# print(type(c))
+
+# c = bool(c)
+# print(type(c))
+# print(c)
+
+
+v = 'fdsfs'
+v = int(v)
